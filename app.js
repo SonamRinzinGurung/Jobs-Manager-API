db6ff02c78971255abdb4120a700d98bc8f37225
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-07-21 12:42:07 +0545

    swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 //connectDB
@@ -33,8 +38,11 @@ app.use(cors());
 app.use(xss());
 
 app.get("/", (req, res) => {
-  res.send("jobs api (testing)");
+  res.send("<h1>Jobs API</h1> <a href='/api-docs'>Documentation</a>");
 });
+
+app.use("./api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
